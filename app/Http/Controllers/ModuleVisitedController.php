4af9646f8721aeb10ede84b2014242cd40d067dd
hakampong <hakamkam.01@gmail.com>
2024-06-19 18:31:37 +0800

    created table for modulevisited

---
+++ app/Http/Controllers/ModuleVisitedController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ModuleVisited;
+use Illuminate\Http\Request;
+
+class ModuleVisitedController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ModuleVisited $moduleVisited)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ModuleVisited $moduleVisited)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ModuleVisited $moduleVisited)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ModuleVisited $moduleVisited)
+    {
+        //
+    }
+}
